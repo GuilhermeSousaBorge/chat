85e9bd88a2b1350f1e3befacfb73ad9bb681c33b
GuilhermeSousaBorge <guilhermepllay157@gmail.com>
2022-06-24 17:14:00 -0300

    Problemas com as consultas

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const db = require("./db.js")
 const path = require('path')
 const { Socket } = require('socket.io')
 const app = express()
@@ -16,17 +16,38 @@ app.use('/',(req, res)=>{
     res.render('index.html')
 })
 
-let messages = []
+//let messages = []
 
 io.on('connection', socket => {
-    console.log(`Socket Conectado ${socket.id}`)
+    console.log(`Socket Conectado ${socket.id}`);
 
-    socket.emit('previousMessage', messages)
+    (async ()=>{
+        const ms = await db.selectMessage();
+        socket.emit('previousMessage', ms)
+        // for(messages of ms){
+
+        // }
+    })();
+
+
 
     socket.on('sendMessage', data => {
-        console.log(data)
-        messages.push(data)
-        socket.broadcast.emit('receivedMessage',data)
+        console.log(data.author)
+        //messages.push(data)
+        console.log('Começou!');
+        console.log('INSERT INTO CLIENTES');
+        (async () =>{
+            const result = await db.insertMessage({author: data.author, message: data.message, color: data.color});
+            const last = await db.selectLastMessage()
+            var messageObject = {
+                author: last[0].author,
+                message: last[0].message,
+                color: last[0].color
+            }
+            console.log("sonsole dentro do server"+messageObject)
+            socket.broadcast.emit('receivedMessage',messageObject);
+        })()
+        
     })
 })
 
